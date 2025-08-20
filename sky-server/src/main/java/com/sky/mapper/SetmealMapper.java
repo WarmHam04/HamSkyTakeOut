eab2110cef252d1e456ca87af252b82352503f66
warmham04 <warmham04@gmail.com>
2025-08-20 19:44:22 +0800

    工作台功能代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -70,4 +71,6 @@ public interface SetmealMapper {
      * @param setmeal
      */
     void insert(Setmeal setmeal);
+
+    Integer countByMap(Map map);
 }
